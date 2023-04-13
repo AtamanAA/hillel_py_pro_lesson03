59c758a52d5bb091177a09ca99a483c9c6237deb
ataman <atamanenkoalex90@gmail.com>
2023-04-13 20:50:41 +0300

    Add PATH: /mean/

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from faker import Faker
+import statistics
+import csv
+
 
 app = Flask(__name__)
 
@@ -33,6 +36,31 @@ def users_generate():
         fake_users.append({"id": number + 1, "name": name, "email": email})
 
     return render_template('users_generate.html', fake_users=fake_users, count=count)
+
+
+@app.route("/mean", methods=['GET'])
+def mean():
+    height_list = []
+    weight_list = []
+
+    with open('hw.csv', newline='') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            if row:
+                try:
+                    height_list.append(float(row[1]))
+                    weight_list.append(float(row[2]))
+                except ValueError:
+                    continue
+
+    mean_height_cm = round(2.54 * statistics.mean(height_list), 1)
+    mean_weight_kg = round(0.453592 * statistics.mean(weight_list), 1)
+    count_people = len(height_list)
+
+    return render_template('mean.html',
+                           mean_height_cm=mean_height_cm,
+                           mean_weight_kg=mean_weight_kg,
+                           count_people=count_people)
 
 
 if __name__ == "__main__":
