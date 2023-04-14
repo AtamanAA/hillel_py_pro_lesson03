b5afe64d3609097b6af43574356cdada2c402530
ataman <atamanenkoalex90@gmail.com>
2023-04-14 13:42:18 +0300

    Update routes

--- main.py
+++ main.py
@@ -13,14 +13,14 @@ def index():
     return render_template('index.html')
 
 
-@app.route("/requirements")
+@app.route("/requirements/")
 def requirements():
     with open("requirements.txt", encoding='utf-16', mode="r") as file:
         requirements_list = [line for line in file]
     return render_template('requirements.html', requirements_list=requirements_list)
 
 
-@app.route("/users/generate", methods=['POST', 'GET'])
+@app.route("/users/generate/", methods=['POST', 'GET'])
 def users_generate():
     if request.method == 'POST':
         count = int(request.form['count'])
@@ -37,7 +37,7 @@ def users_generate():
     return render_template('users_generate.html', fake_users=fake_users, count=count)
 
 
-@app.route("/mean", methods=['GET'])
+@app.route("/mean/", methods=['GET'])
 def mean():
     height_list = []
     weight_list = []
@@ -62,7 +62,7 @@ def mean():
                            count_people=count_people)
 
 
-@app.route("/space")
+@app.route("/space/")
 def space():
     r = requests.get('http://api.open-notify.org/astros.json')
     astro_data = r.json()
